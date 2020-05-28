3f43f29a32d5907e32ed848a24206b5660084c6b
Judith Huynh <judithuynh@gmail.com>
2020-05-28 22:02:50 +0000

    displays a count for the number of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,13 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["SECRET_USER"], password: ENV["SECRET_PASSWORD"]
   
   def show
+    category_count
+    product_count
   end
+  def category_count 
+    @category = Category.count
+  end 
+  def product_count
+    @product = Product.count
+  end 
 end
