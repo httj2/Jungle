510683942b2d70a82e83390e21efedd53cf92786
Judith Huynh <judithuynh@gmail.com>
2020-05-28 13:28:39 +0000

    cart will display a message when empty with a link to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,9 @@ class ApplicationController < ActionController::Base
   end
   helper_method :enhanced_cart
 
+  # def load_cart
+  #   @order = cart_subtotal_cents
+  # end 
   def cart_subtotal_cents
     enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
   end
